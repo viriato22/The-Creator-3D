c13309eee8ebbac4581032311201b452ee4f3020
Daraenor <viriato22@gmail.com>
2017-12-13 22:30:33 +0100

    Updated UI

--- Source/PhysicsWindow.cpp
+++ Source/PhysicsWindow.cpp
@@ -21,12 +21,44 @@ PhysicsWindow::~PhysicsWindow()
 void PhysicsWindow::DrawWindow()
 {
 	if (ImGui::BeginDock(window_name.c_str(), false, false, App->IsPlaying(), ImGuiWindowFlags_HorizontalScrollbar)) {
-		GameObject* selected_gameobject = nullptr;
+		GameObject* object1 = nullptr;
+		GameObject* object2 = nullptr;
 		if (App->scene->selected_gameobjects.size() == 1) {
-			selected_gameobject = App->scene->selected_gameobjects.front();
+			if (ImGui::Button("Make physbody")) {
+
+			}
+
+			if (ImGui::Button("Select GameObject")) {
+				if (object1 == nullptr) {
+					object1 = App->scene->selected_gameobjects.front();
+				}
+				else if (object2 == nullptr && App->scene->selected_gameobjects.front() != object1) {
+					object2 = App->scene->selected_gameobjects.front();
+				}
+				else {
+					//error message
+				}
+			}
 		}
 		else if (App->scene->selected_gameobjects.size() == 2) {
-			//Should give possibility to make hinges
+			//Maybe make this only accessible if they are not alredy constrained between eachother
+			if (ImGui::Button("Make constraint")) {
+				std::list<GameObject*>::iterator ptr; //This code is to select each gameobject
+				ptr = App->scene->selected_gameobjects.begin();
+				object1 = (GameObject*)(*ptr);
+				ptr++;
+				object2 = (GameObject*)(*ptr);
+			}
+			if (ImGui::Button("Make a hinge")) {
+				std::list<GameObject*>::iterator ptr;
+				ptr = App->scene->selected_gameobjects.begin();
+				object1 = (GameObject*)(*ptr);
+				ptr++;
+				object2 = (GameObject*)(*ptr);
+			}
+		}
+		else {
+
 		}
 	}
 	ImGui::EndDock();
