c79623100af32e14f9a3ef364c4ec621f00f1167
Daraenor <viriato22@gmail.com>
2017-12-14 20:58:10 +0100

    Added component windows for all colliders

--- Source/PhysicsWindow.cpp
+++ Source/PhysicsWindow.cpp
@@ -27,6 +27,10 @@ void PhysicsWindow::DrawWindow()
 		if (App->scene->selected_gameobjects.size() == 1) {
 			object1 = App->scene->selected_gameobjects.front();
 			if (object1 != nullptr && !object1->is_on_destroy) {
+				for (std::list<Component*>::iterator it = object1->components_list.begin(); it != object1->components_list.end(); it++) {
+					DrawComponent((*it));
+				}
+
 				if (ImGui::Button("Add Collider")) {
 					ImGui::OpenPopup("Colliders");
 				}
@@ -42,10 +46,6 @@ void PhysicsWindow::DrawWindow()
 						}
 					}
 					ImGui::EndPopup();
-				}
-
-				for (std::list<Component*>::iterator it = object1->components_list.begin(); it != object1->components_list.end(); it++) {
-					DrawComponent((*it));
 				}
 			}
 		}
@@ -113,6 +113,8 @@ void PhysicsWindow::DrawRigidbodyPanel(ComponentRigidBody* rigidbody) {
 			ImGui::Checkbox("Y", &FreezePosY); ImGui::SameLine();
 			ImGui::Checkbox("Z", &FreezePosZ);
 
+			ImGui::Spacing();
+
 			ImGui::Text("Freeze rotation"); ImGui::SameLine();
 			ImGui::Checkbox("X", &FreezeRotX); ImGui::SameLine();
 			ImGui::Checkbox("Y", &FreezeRotY); ImGui::SameLine();
@@ -120,3 +122,113 @@ void PhysicsWindow::DrawRigidbodyPanel(ComponentRigidBody* rigidbody) {
 		}
 	}
 }
+
+void PhysicsWindow::DrawBoxColliderPanel()
+{
+	bool trigger;
+	float3 center, size;
+
+	if (ImGui::CollapsingHeader("Box Collider")) {
+		ImGui::Checkbox("Is Trigger", &trigger);
+
+		ImGui::Spacing();
+
+		ImGui::DragFloat3("Center", (float*)&center, 0.25f);
+		ImGui::DragFloat3("Size", (float*)&size, 0.25f);
+	}
+}
+
+void PhysicsWindow::DrawSphereColliderPanel()
+{
+	bool trigger;
+	float3 center;
+	float radius;
+
+	if (ImGui::CollapsingHeader("Sphere Collider")) {
+		ImGui::Checkbox("Is Trigger", &trigger);
+
+		ImGui::Spacing();
+
+		ImGui::DragFloat3("Center", (float*)&center, 0.25f);
+		ImGui::DragFloat("Radius", &radius, 0.25f);
+	}
+}
+
+void PhysicsWindow::DrawCapsuleColliderPanel()
+{
+	bool trigger;
+	float3 center;
+	float radius, height;
+
+	if (ImGui::CollapsingHeader("Capsule Collider")) {
+		ImGui::Checkbox("Is Trigger", &trigger);
+
+		ImGui::Spacing();
+
+		ImGui::DragFloat3("Center", (float*)&center, 0.25f);
+		ImGui::DragFloat("Radius", &radius, 0.25f);
+		ImGui::DragFloat("Height", &height, 0.25f);
+	}
+}
+
+void PhysicsWindow::DrawWheelColliderPanel()
+{
+	float mass, radius, wheeldamp, susprate, forceapp;
+	float3 center;
+	float spring, damper, targetpos;
+	float ffexslip, ffexval, ffaslip, ffasval, ffstiffness;
+	float sfexslip, sfexval, sfaslip, sfasval, sfstiffness;
+
+	if (ImGui::CollapsingHeader("Wheel Collider")) {
+		ImGui::DragFloat("Mass", &mass, 0.25f);
+		ImGui::DragFloat("Radius", &radius, 0.25f);
+		ImGui::DragFloat("Wheel Damping Rate", &wheeldamp, 0.25f);
+		ImGui::DragFloat("Suspension Rate", &susprate, 0.05f);
+		ImGui::DragFloat("Force App Point Distance", &forceapp, 0.25f);
+
+		ImGui::DragFloat3("Center", (float*)&center, 0.25f);
+
+		if (ImGui::CollapsingHeader("Suspension Spring")) {
+			ImGui::DragFloat("Spring", &spring, 0.25f);
+			ImGui::DragFloat("Damper", &damper, 0.25f);
+			ImGui::DragFloat("Target Position", &targetpos, 0.05f);
+		}
+
+		if (ImGui::CollapsingHeader("Forward Friction")) {
+			ImGui::DragFloat("Extremum Slip", &ffexslip, 0.05f);
+			ImGui::DragFloat("Extremum Value", &ffexval, 0.05f);
+			ImGui::DragFloat("Asymptote Slip", &ffaslip, 0.05f);
+			ImGui::DragFloat("Asymptote Value", &ffasval, 0.05f);
+			ImGui::DragFloat("Stiffness", &ffstiffness, 0.1f);
+		}
+
+		if (ImGui::CollapsingHeader("Sideways Friction")) {
+			ImGui::DragFloat("Extremum Slip", &sfexslip, 0.05f);
+			ImGui::DragFloat("Extremum Value", &sfexval, 0.05f);
+			ImGui::DragFloat("Asymptote Slip", &sfaslip, 0.05f);
+			ImGui::DragFloat("Asymptote Value", &sfasval, 0.05f);
+			ImGui::DragFloat("Stiffness", &sfstiffness, 0.1f);
+		}
+	}
+}
+
+//void PhysicsWindow::DrawHingeConstraintPanel()
+//{
+//	if (ImGui::CollapsingHeader("Hinge Constraint")) {
+//
+//	}
+//}
+//
+//void PhysicsWindow::DrawFixedConstraintPanel()
+//{
+//	if (ImGui::CollapsingHeader("Fixed Constraint")) {
+//
+//	}
+//}
+//
+//void PhysicsWindow::DrawSpringConstraintPanel()
+//{
+//	if (ImGui::CollapsingHeader("Spring Constraint")) {
+//
+//	}
+//}
--- Source/PhysicsWindow.h
+++ Source/PhysicsWindow.h
@@ -15,7 +15,16 @@ public:
 	void DrawWindow();
 
 	void DrawComponent(Component * component);
+
 	void DrawRigidbodyPanel(ComponentRigidBody* rigidbody);
+	void DrawBoxColliderPanel();
+	void DrawSphereColliderPanel();
+	void DrawCapsuleColliderPanel();
+	void DrawWheelColliderPanel();
+
+	/*void DrawHingeConstraintPanel();
+	void DrawFixedConstraintPanel();
+	void DrawSpringConstraintPanel();*/
 
 private:
 	//void SelectObjects(GameObject* object1, GameObject* object2); //For some reason it doesn't work
